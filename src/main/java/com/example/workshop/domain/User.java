179acf54e361d08a7f7d42d12a9e5e614b85754c
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-12 17:01:38 -0300

    Conectando ao MongoDB com repository e services

--- src/main/java/com/example/workshop/domain/User.java
+++ src/main/java/com/example/workshop/domain/User.java
@@ -1,6 +1,11 @@
 package com.example.workshop.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class User {
+    @Id
     private String id;
     private String name;
     private String email;
