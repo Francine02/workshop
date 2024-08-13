aa1071161b1f0264595106da66663b576085b39a
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-13 18:38:30 -0300

    Referênciando posts do user

--- src/main/java/com/example/workshop/domain/User.java
+++ src/main/java/com/example/workshop/domain/User.java
@@ -1,6 +1,10 @@
 package com.example.workshop.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -9,6 +13,9 @@ public class User {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true) //Faz referência. Lazzy é se os posts seram carregados somente se acessados
+    private List<Post> posts = new ArrayList<>();
 
     public User(){}
 
@@ -40,6 +47,14 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
