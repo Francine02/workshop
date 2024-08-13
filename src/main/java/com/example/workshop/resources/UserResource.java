59222d28de43dbb7746a0a39b96d50bef9e827b9
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-08-13 18:41:43 -0300

    Endpoint para retornar os posts

--- src/main/java/com/example/workshop/resources/UserResource.java
+++ src/main/java/com/example/workshop/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.workshop.domain.Post;
 import com.example.workshop.domain.User;
 import com.example.workshop.dto.UserDTO;
 import com.example.workshop.services.UserService;
@@ -63,5 +64,12 @@ public class UserResource {
         user.setId(id);
         user = service.update(user);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User user = service.findById(id);
+
+        return ResponseEntity.ok().body(user.getPosts());
     }
 }
